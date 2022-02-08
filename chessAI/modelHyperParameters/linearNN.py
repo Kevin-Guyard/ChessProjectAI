3ff4ea66f5f3dbafe206ed0c79dd534fc8245e56
Kevin <kevin.guyard@live.fr>
2022-02-08 02:15:44 +0100

    add constant

--- chessAI/modelHyperParameters/linearNN.py
+++ chessAI/modelHyperParameters/linearNN.py
@@ -1,6 +1,9 @@
 import random
 from scipy.stats import loguniform
 
+SIZE_MIN_LAYER = 1e0
+SIZE_MAX_LAYER = 1e4
+
 def get_model_parameters_LinearNN(model_name):
         
     nb_hidden_layers = int(model_name.split('LinearNN')[1])
@@ -12,7 +15,7 @@ def get_model_parameters_LinearNN(model_name):
     }
     
     for n_layer in range(0, nb_hidden_layers):
-        model_parameters['size_layer_' + str(n_layer + 1)] = int(loguniform.rvs(1e0, 1e4, size=1)[0])
+        model_parameters['size_layer_' + str(n_layer + 1)] = int(loguniform.rvs(SIZE_MIN_LAYER, SIZE_MAX_LAYER, size=1)[0])
         model_parameters['activation_' + str(n_layer)] = 'LeakyReLU'
         model_parameters['activation_slope_' + str(n_layer)] = 0.1
         model_parameters['dropout_' + str(n_layer)] = random.random()
