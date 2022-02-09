46506a32a0fcf7f532bd96cf39710115d6cf3d07
Kevin <kevin.guyard@live.fr>
2022-02-09 01:06:53 +0100

    change model and hyperparameters to list for CNN

--- chessAI/modelHyperParameters/cNN.py
+++ chessAI/modelHyperParameters/cNN.py
@@ -25,7 +25,20 @@ def get_model_parameters_CNN(model_name):
         'model_name': model_name,
         'nb_conv_layers': nb_conv_layers,
         'nb_linear_layers': nb_linear_layers,
-        'channels_0': 12
+        'channels': [12],
+        'kernel_conv': [],
+        'stride_conv': [],
+        'padding_conv': [],
+        'is_conv_bis': [],
+        'is_pool_max': [],
+        'kernel_pool': [],
+        'stride_pool': [],
+        'padding_pool': [],
+        'dropout_conv': [],
+        'batchnorm_conv': [],
+        'size_linear_layer': [],
+        'dropout_linear': [],
+        'batchnorm_linear': []
     }
     
     channels = 12
@@ -44,13 +57,36 @@ def get_model_parameters_CNN(model_name):
                 kernel_conv -= 1
         stride_conv = random.randint(STRIDE_MIN_CONV, STRIDE_MAX_CONV)
         padding_conv = random.randint(PADDING_MIN_CONV, min(PADDING_MAX_CONV, int(kernel_conv/2)))
-        
-        model_parameters['channels_' + str(n_conv_layer + 1)] = channels
-        model_parameters['kernel_conv_' + str(n_conv_layer)] = kernel_conv
-        model_parameters['stride_conv_' + str(n_conv_layer)] = stride_conv
-        model_parameters['padding_conv_' + str(n_conv_layer)] = padding_conv
-        
+    
+        model_parameters['channels'].append(channels) 
+        model_parameters['kernel_conv'].append(kernel_conv)
+        model_parameters['stride_conv'].append(stride_conv)
+        model_parameters['padding_conv'].append(padding_conv)
+
         dim = int((dim - kernel_conv + 2 * padding_conv) / stride_conv) + 1
+        
+        is_conv_bis = bool(random.randint(0, 1))
+        model_parameters['is_conv_bis'].append(is_conv_bis)
+        
+        if is_conv_bis == True:
+            
+            channels = random.randint(channels, NB_MAX_CHANNELS)
+            kernel_conv = random.randint(kernel_conv, SIZE_MAX_KERNEL_CONV)
+            if kernel_conv % 2 == 0:
+                kernel_conv += 1
+            if kernel_conv > dim:
+                kernel_conv = dim
+                if kernel_conv % 2 == 0:
+                    kernel_conv -= 1
+            stride_conv = random.randint(STRIDE_MIN_CONV, STRIDE_MAX_CONV)
+            padding_conv = random.randint(PADDING_MIN_CONV, min(PADDING_MAX_CONV, int(kernel_conv/2)))
+
+            model_parameters['channels'].append(channels) 
+            model_parameters['kernel_conv'].append(kernel_conv)
+            model_parameters['stride_conv'].append(stride_conv)
+            model_parameters['padding_conv'].append(padding_conv)
+            
+            dim = int((dim - kernel_conv + 2 * padding_conv) / stride_conv) + 1
         
         is_pool_max = bool(random.randint(0, 1))
         kernel_pool = random.randint(SIZE_MIN_KERNEL_POOL, SIZE_MAX_KERNEL_POOL)
@@ -59,27 +95,29 @@ def get_model_parameters_CNN(model_name):
         stride_pool = random.randint(STRIDE_MIN_POOL, STRIDE_MAX_POOL)
         padding_pool = random.randint(PADDING_MIN_POOL, min(PADDING_MAX_POOL, int(kernel_pool/2)))
         
-        model_parameters['is_pool_max_' + str(n_conv_layer)] = is_pool_max
-        model_parameters['kernel_pool_' + str(n_conv_layer)] = kernel_pool
-        model_parameters['stride_pool_' + str(n_conv_layer)] = stride_pool
-        model_parameters['padding_pool_' + str(n_conv_layer)] = padding_pool
+        model_parameters['is_pool_max'].append(is_pool_max) 
+        model_parameters['kernel_pool'].append(kernel_pool) 
+        model_parameters['stride_pool'].append(stride_pool) 
+        model_parameters['padding_pool'].append(padding_pool) 
         
         dim = int((dim - kernel_pool + 2 * padding_pool) / stride_pool) + 1
         
-        dropout = random.random()
-        batchnorm = bool(random.randint(0, 1))
+        dropout_conv = random.random()
+        batchnorm_conv = bool(random.randint(0, 1))
         
-        model_parameters['dropout_conv_' + str(n_conv_layer)] = dropout
-        model_parameters['batchnorm_conv_' + str(n_conv_layer)] = batchnorm
+        model_parameters['dropout_conv'].append(dropout_conv)
+        model_parameters['batchnorm_conv'].append(batchnorm_conv)
         
-    model_parameters['size_linear_layer_0'] = dim * dim * channels
+    model_parameters['size_linear_layer'].append(dim * dim * channels)
     
     for n_linear_layer in range(0, nb_linear_layers):
         
-        model_parameters['size_linear_layer_' + str(n_linear_layer + 1)] = int(loguniform.rvs(SIZE_MIN_LINEAR_LAYER, SIZE_MAX_LINEAR_LAYER, size=1)[0])
-        model_parameters['activation_linear_' + str(n_linear_layer)] = 'LeakyReLU'
-        model_parameters['activation_slope_linear_' + str(n_linear_layer)] = 0.1
-        model_parameters['dropout_linear_' + str(n_linear_layer)] = random.random()
-        model_parameters['batchnorm_linear_' + str(n_linear_layer)] = bool(random.randint(0, 1))
-            
+        size_linear_layer = int(loguniform.rvs(SIZE_MIN_LINEAR_LAYER, SIZE_MAX_LINEAR_LAYER, size=1)[0])
+        dropout_linear = random.random()
+        batchnorm_linear = bool(random.randint(0, 1))
+        
+        model_parameters['size_linear_layer'].append(size_linear_layer)
+        model_parameters['dropout_linear'].append(dropout_linear)
+        model_parameters['batchnorm_linear'].append(batchnorm_linear)
+    
     return model_parameters
--- chessAI/models/cNN.py
+++ chessAI/models/cNN.py
@@ -2,47 +2,75 @@ import torch
 
 class CNN(torch.nn.Module):
     
-    def __init__(self, parameters=None):
+    def __init__(self, parameters):
         
         super(CNN, self).__init__()
         
-        layers = []
+        net = []
+        
+        in_channels = parameters['channels'].pop(0)
         
         for n_conv_layer in range(0, parameters['nb_conv_layers']):
-            layers.append(torch.nn.Conv2d(in_channels=parameters['channels_' + str(n_conv_layer)], \
-                                          out_channels=parameters['channels_' + str(n_conv_layer + 1)], \
-                                          kernel_size=parameters['kernel_conv_' + str(n_conv_layer)], \
-                                          stride=parameters['stride_conv_' + str(n_conv_layer)], \
-                                          padding=parameters['padding_conv_' + str(n_conv_layer)]))
-            layers.append(torch.nn.ReLU())
-            if parameters['batchnorm_conv_' + str(n_conv_layer)] == True:
-                layers.append(torch.nn.BatchNorm2d(num_features=parameters['channels_' + str(n_conv_layer + 1)]))
-            if parameters['is_pool_max_' + str(n_conv_layer)] == True:
-                layers.append(torch.nn.MaxPool2d(kernel_size=parameters['kernel_pool_' + str(n_conv_layer)], \
-                                                 stride=parameters['stride_pool_' + str(n_conv_layer)], \
-                                                 padding=parameters['padding_pool_' + str(n_conv_layer)]))
+            
+            conv_layers = []
+            out_channels = parameters['channels'].pop(0)
+            
+            conv_layers.append(torch.nn.Conv2d(in_channels=in_channels, \
+                                               out_channels=out_channels, \
+                                               kernel_size=parameters['kernel_conv'].pop(0), \
+                                               stride=parameters['stride_conv'].pop(0), \
+                                               padding=parameters['padding_conv'].pop(0)))
+            if parameters['is_conv_bis'].pop(0) == True:
+                in_channels = out_channels
+                out_channels = parameters['channels'].pop(0)
+                conv_layers.append(torch.nn.Conv2d(in_channels=in_channels, \
+                                                   out_channels=out_channels, \
+                                                   kernel_size=parameters['kernel_conv'].pop(0), \
+                                                   stride=parameters['stride_conv'].pop(0), \
+                                                   padding=parameters['padding_conv'].pop(0)))
+            conv_layers.append(torch.nn.ReLU())
+            if parameters['batchnorm_conv'].pop(0) == True:
+                conv_layers.append(torch.nn.BatchNorm2d(num_features=out_channels))
+            if parameters['is_pool_max'].pop(0) == True:
+                conv_layers.append(torch.nn.MaxPool2d(kernel_size=parameters['kernel_pool'].pop(0), \
+                                                      stride=parameters['stride_pool'].pop(0), \
+                                                      padding=parameters['padding_pool'].pop(0)))
             else:
-                layers.append(torch.nn.AvgPool2d(kernel_size=parameters['kernel_pool_' + str(n_conv_layer)], \
-                                                 stride=parameters['stride_pool_' + str(n_conv_layer)], \
-                                                 padding=parameters['padding_pool_' + str(n_conv_layer)]))
-            layers.append(torch.nn.Dropout(parameters['dropout_conv_' + str(n_conv_layer)]))
+                conv_layers.append(torch.nn.AvgPool2d(kernel_size=parameters['kernel_pool'].pop(0), \
+                                                      stride=parameters['stride_pool'].pop(0), \
+                                                      padding=parameters['padding_pool'].pop(0)))
+            conv_layers.append(torch.nn.Dropout(parameters['dropout_conv'].pop(0)))
+            
+            conv = torch.nn.Sequential(*conv_layers)
+            net.append(conv)
+            
+            in_channels = out_channels
+            
+        net.append(torch.nn.Flatten())
         
-        layers.append(torch.nn.Flatten())
+        in_features = parameters['size_linear_layer'].pop(0)
         
         for n_linear_layer in range(0, parameters['nb_linear_layers']):
-            layers.append(torch.nn.Linear(in_features=parameters['size_linear_layer_' + str(n_linear_layer)], out_features=parameters['size_linear_layer_' + str(n_linear_layer + 1)]))
-            if parameters['activation_linear_' + str(n_linear_layer)] == 'ReLU':
-                layers.append(torch.nn.ReLU())
-            elif parameters['activation_linear_' + str(n_linear_layer)] == 'LeakyReLU':
-                layers.append(torch.nn.LeakyReLU(negative_slope=parameters['activation_slope_linear_' + str(n_linear_layer)]))
-            layers.append(torch.nn.Dropout(parameters['dropout_linear_' + str(n_linear_layer)]))
-            if parameters['batchnorm_linear_' + str(n_linear_layer)] == True:
-                layers.append(torch.nn.BatchNorm1d(parameters['size_linear_layer_' + str(n_linear_layer + 1)]))
+            
+            linear_layers = []
+            out_features = parameters['size_linear_layer'].pop(0)
+            
+            linear_layers.append(torch.nn.Linear(in_features=in_features, \
+                                                 out_features=out_features))
+            linear_layers.append(torch.nn.ReLU())
+            linear_layers.append(torch.nn.Dropout(parameters['dropout_linear'].pop(0)))
+            if parameters['batchnorm_linear'].pop(0) == True:
+                linear_layers.append(torch.nn.BatchNorm1d(out_features))
+                
+            linear = torch.nn.Sequential(*linear_layers)
+            net.append(linear)
+            
+            in_features = out_features
+            
+        net.append(torch.nn.Linear(in_features=in_features, out_features=1))
+        net.append(torch.nn.Sigmoid())
         
-        layers.append(torch.nn.Linear(in_features=parameters['size_linear_layer_' + str(parameters['nb_linear_layers'])], out_features=1))
-        layers.append(torch.nn.Sigmoid())
-        
-        self._net = torch.nn.Sequential(*layers)
+        self._net = torch.nn.Sequential(*net)
         
         
     def forward(self, x):
